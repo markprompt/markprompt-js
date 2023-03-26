ae0ae62f5eac2a4c3afdb3a802b5a39b24b8a78b
Michael Fester <michael.fester@gmail.com>
2023-03-26 09:38:03 -0700

    Update banner

--- components/layouts/NavLayout.tsx
+++ components/layouts/NavLayout.tsx
@@ -9,10 +9,10 @@ type NavLayoutProps = {
 export const NavLayout: FC<NavLayoutProps> = ({ animated, children }) => {
   return (
     <div className="relative min-h-screen w-full">
-      <div className="fixed inset-x-0 top-0 z-50 flex h-8 w-full items-center justify-center overflow-x-auto truncate whitespace-nowrap bg-fuchsia-900/30 px-4 text-center text-xs text-fuchsia-600">
+      <div className="fixed inset-x-0 top-0 z-50 flex h-8 w-full items-center justify-center overflow-x-auto truncate whitespace-nowrap bg-fuchsia-900/30 px-4 text-center text-xs text-fuchsia-500">
         Heads up! Due to unexpected high demand, we are hitting some rate
-        limits, and are currently limiting training to 20 files. You may
-        experience empty prompt results occasionally. We are addressing this.
+        limits. You may experience empty prompt results occasionally. We are
+        addressing this.
       </div>
       <AppNavbar animated={animated} />
       <div className="pb-12">{children}</div>
