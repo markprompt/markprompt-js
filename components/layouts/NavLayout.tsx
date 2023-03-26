49f731138f0ad542240d904a1ceb4b080dd18cf1
Michael Fester <michael.fester@gmail.com>
2023-03-26 16:38:16 -0700

    Update message

--- components/layouts/NavLayout.tsx
+++ components/layouts/NavLayout.tsx
@@ -11,7 +11,7 @@ export const NavLayout: FC<NavLayoutProps> = ({ animated, children }) => {
     <div className="relative min-h-screen w-full">
       <div className="fixed inset-x-0 top-0 z-50 flex h-8 w-full items-center justify-center overflow-x-auto truncate whitespace-nowrap bg-fuchsia-900/30 px-4 text-center text-xs text-fuchsia-500">
         Heads up! Due to unexpected high demand, we are hitting some rate
-        limits. You may experience empty prompt results occasionally. We are
+        limits. You may experience this when syncing a GitHub repo. We are
         addressing this.
       </div>
       <AppNavbar animated={animated} />
