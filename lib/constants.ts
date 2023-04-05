360bb97aac89ae954ad7e3d6ddb9aadc3229990f
Michael Fester <michael.fester@gmail.com>
2023-04-05 16:34:10 -0700

    Update token cutoff value

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,4 +2,4 @@ export const I_DONT_KNOW = 'Sorry, I am not sure how to answer that.';
 export const MIN_CONTENT_LENGTH = 20;
 export const MAX_PROMPT_LENGTH = 200;
 export const STREAM_SEPARATOR = '___START_RESPONSE_STREAM___';
-export const CONTEXT_TOKENS_CUTOFF = 1200;
+export const CONTEXT_TOKENS_CUTOFF = 4000;
