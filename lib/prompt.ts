a7248b36effda74fb05cf71f94d72516bb4699b1
Michael Fester <michael.fester@gmail.com>
2023-04-13 19:09:11 -0700

    Update prompt

--- lib/prompt.ts
+++ lib/prompt.ts
@@ -1,7 +1,7 @@
 import { oneLine, stripIndent } from 'common-tags';
 
 export const DEFAULT_PROMPT_TEMPLATE = stripIndent`
-  ${oneLine`You are a very enthusiastic company representative who loves to help people! Given the following sections from the documentation (preceded by a section id), answer the question using only that information, outputted in Markdown format. If you are unsure and the answer is not explicitly written in the documentation, say "{{I_DONT_KNOW}}"`}
+  ${oneLine`You are a very enthusiastic company representative who loves to help people! Given the following sections from the documentation (preceded by a section id), answer the question using only that information, outputted in Markdown format. If you are unsure and the answer is not explicitly written in the documentation, say "{{I_DONT_KNOW}}".`}
 
 Context sections:
 ---
