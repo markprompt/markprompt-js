c1761e3cff8cca7b29f2c517e3cd3bb882ceb213
Remco Haszing <remcohaszing@gmail.com>
2023-04-22 15:08:29 +0200

    Simplify examples

--- examples/with-tailwind/src/App.tsx
+++ examples/with-tailwind/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="bg-sky-500 text-3xl font-bold underline">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
