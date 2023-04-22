9025a0e3276e417f9d863adf0a3b83803496a1dc
Remco Haszing <remcohaszing@gmail.com>
2023-04-22 13:21:31 +0200

    Fix ESLint issues

--- examples/with-stitches/src/App.tsx
+++ examples/with-stitches/src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+
+import reactLogo from './assets/react.svg';
+
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -29,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
