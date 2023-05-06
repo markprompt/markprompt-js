8937270889204175f81152efa69f0ebc2c506efd
Nick Rutten <2504906+nickrttn@users.noreply.github.com>
2023-05-06 13:40:34 +0100

    Add ESLint config for @markprompt/react
    Adds `eslint-plugin-react` and `eslint-plugin-react-hooks`

---
+++ packages/react/.eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  root: false,
+  extends: ['plugin:react/recommended', 'plugin:react-hooks/recommended'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
