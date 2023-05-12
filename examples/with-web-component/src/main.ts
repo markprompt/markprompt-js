3326036810ec4d7470551317099e1d3b70d2fe0d
Michael Fester <michael.fester@gmail.com>
2023-05-12 10:32:51 -0700

    Remove API key

--- examples/with-web-component/src/main.ts
+++ examples/with-web-component/src/main.ts
@@ -2,6 +2,8 @@ import './style.css';
 import '@markprompt/web';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <p>press the button ↘️</p>
-  <markprompt-root project-key="sk_test_mKfzAaRVAZaVvu0MHJvGNJBywfJSOdp4" model="gpt-4"></markprompt-root>
+  <p>Press the button ↘️</p>
+  <markprompt-root project-key="${
+    import.meta.env.VITE_PROJECT_API_KEY
+  }" model="gpt-4"></markprompt-root>
 `;
