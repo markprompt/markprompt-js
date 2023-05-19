9efc9188b53b5b98a7ac08966c02ccb621936b34
Remco Haszing <remcohaszing@gmail.com>
2023-05-19 16:20:03 +0200

    Add tests for @markprompt/react

---
+++ packages/react/src/useMarkprompt.test.ts
@@ -0,0 +1,81 @@
+import { MARKPROMPT_COMPLETIONS_URL, STREAM_SEPARATOR } from '@markprompt/core';
+import { act, waitFor } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { afterAll, afterEach, beforeAll, describe, expect, test } from 'vitest';
+
+import { useMarkprompt } from './useMarkprompt.js';
+
+const encoder = new TextEncoder();
+let response: string[] = [];
+let status = 200;
+let stream: ReadableStream;
+const server = setupServer(
+  rest.post(MARKPROMPT_COMPLETIONS_URL, async (req, res, ctx) => {
+    stream = new ReadableStream({
+      start(controller) {
+        for (const chunk of response) {
+          controller.enqueue(encoder.encode(chunk));
+        }
+        controller?.close();
+      },
+    });
+    return res(ctx.status(status), ctx.body(stream));
+  }),
+);
+
+beforeAll(() => {
+  server.listen({ onUnhandledRequest: 'error' });
+});
+
+afterAll(() => {
+  server.close();
+});
+
+afterEach(() => {
+  response = [];
+  status = 200;
+  server.resetHandlers();
+});
+
+describe('useMarkprompt', () => {
+  test('initial state', () => {
+    const { result } = renderHook(() =>
+      useMarkprompt({ projectKey: 'TEST_PROJECT_KEY' }),
+    );
+
+    expect(result.current).toStrictEqual({
+      answer: '',
+      references: [],
+      state: 'indeterminate',
+      prompt: '',
+      abort: expect.any(Function),
+      updatePrompt: expect.any(Function),
+      submit: expect.any(Function),
+    });
+  });
+
+  test('submit', async () => {
+    const { result } = renderHook(() =>
+      useMarkprompt({ projectKey: 'TEST_PROJECT_KEY' }),
+    );
+
+    act(() => {
+      result.current.updatePrompt('How much is 1+2?');
+    });
+
+    response = [
+      '["https://calculator.example"]',
+      STREAM_SEPARATOR,
+      'According to my calculator ',
+      '1 + 2 = 3',
+    ];
+    await result.current.submit();
+    await waitFor(() =>
+      expect(result.current.answer).toBe(
+        'According to my calculator 1 + 2 = 3',
+      ),
+    );
+  });
+});
