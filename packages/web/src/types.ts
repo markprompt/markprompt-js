2463536001c801367f661c6f8d4181e7a0df9b05
Nick Rutten <2504906+nickrttn@users.noreply.github.com>
2023-05-21 14:48:54 +0100

    Add proper JSDoc @default tags to MarkpromptOptions,

--- packages/web/src/types.ts
+++ packages/web/src/types.ts
@@ -1,48 +1,76 @@
 import { type Options as CoreOptions } from '@markprompt/core';
 
 type MarkpromptOptions = CoreOptions & {
-  /** Props for the close modal button */
   close?: {
-    /** Aria-label for the close modal button */
+    /**
+     * `aria-label` for the close modal button
+     * @default "Close Markprompt"
+     **/
     label?: string;
   };
-  /** Props for the description */
   description?: {
-    /** Whether to hide the description, default: `true` */
+    /**
+     * Visually hide the description
+     * @default true
+     **/
     hide?: boolean;
-    /** Text for the description */
+    /**
+     * Description text
+     **/
     text?: string;
   };
-  /** Props for the prompt */
   prompt?: {
-    /** Label for the prompt input, default: `Your prompt` */
+    /**
+     * Label for the prompt input
+     * @default "Your prompt"
+     **/
     label?: string;
-    /** Placeholder for the prompt input, default: `Ask me anything…` */
+    /**
+     * Placeholder for the prompt input
+     * @default "Ask me anything…"
+     **/
     placeholder?: string;
   };
   references?: {
-    /** Callback to transform a reference id into an href and text */
+    /**
+     * Callback to transform a reference id into an href and text
+     **/
     transformReferenceId: (referenceId: string) => {
       href: string;
       text: string;
     };
     /** Loading text, default: `Fetching relevant pages…` */
     loadingText?: string;
-    /** References title, default: `Answer generated from the following sources:` */
+    /**
+     * References title
+     * @default "Answer generated from the following sources:"
+     **/
     referencesText?: string;
   };
-  /** Props for the trigger */
   trigger?: {
-    /** Aria-label for the trigger button */
+    /**
+     * `aria-label` for the open button
+     * @default "Open Markprompt"
+     **/
     label?: string;
   };
-  /** Props for the title */
   title?: {
-    /** Whether to hide the title, default: `true` */
+    /**
+     * Visually hide the title
+     * @default true
+     **/
     hide?: boolean;
-    /** Text for the title: default `Ask me anything` */
+    /**
+     * Text for the title
+     * @default "Ask me anything"
+     **/
     text?: string;
   };
+  /**
+   * Show Markprompt branding
+   * @default true
+   **/
+  showBranding?: boolean;
 };
 
 export type { MarkpromptOptions };
