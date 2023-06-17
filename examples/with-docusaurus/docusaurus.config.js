ea8daa16f9fd19db5e9681765079c5ec72f7e9a6
Michael Fester <michael.fester@gmail.com>
2023-06-17 09:46:45 -0700

    Fix dependencies

--- examples/with-docusaurus/docusaurus.config.js
+++ examples/with-docusaurus/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://markprompt-docusaurus.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
