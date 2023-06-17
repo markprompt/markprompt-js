5526040269155ba16d0c708805cebd7c9cade545
Nick Rutten <2504906+nickrttn@users.noreply.github.com>
2023-06-17 12:54:06 +0100

    Fix the examples

--- examples/with-next/pages/index.tsx
+++ examples/with-next/pages/index.tsx
@@ -10,23 +10,25 @@ function IndexPage(): ReactElement {
   return (
     <Markprompt.Root
       projectKey={process.env.MARKPROMPT_PROJECT_KEY!}
-      iDontKnowMessage="Sorry, I am not sure how to answer that."
-      promptTemplate={`You are a very enthusiastic company representative who loves to help people! Given the following sections from the documentation (preceded by a section id), answer the question using only that information, output in Markdown format. If you are unsure and the answer is not explicitly written in the documentation, say "{{I_DONT_KNOW}}".
+      promptOptions={{
+        iDontKnowMessage: 'Sorry, I am not sure how to answer that.',
+        promptTemplate: `You are a very enthusiastic company representative who loves to help people! Given the following sections from the documentation (preceded by a section id), answer the question using only that information, output in Markdown format. If you are unsure and the answer is not explicitly written in the documentation, say "{{I_DONT_KNOW}}".
 
-Context sections:
----
-{{CONTEXT}}
+  Context sections:
+  ---
+  {{CONTEXT}}
 
-Question: "{{PROMPT}}"
+  Question: "{{PROMPT}}"
 
-Answer (including related code snippets if available):`}
-      temperature={0.1}
-      topP={1}
-      frequencyPenalty={0}
-      presencePenalty={0}
-      maxTokens={500}
-      sectionsMatchCount={10}
-      sectionsMatchThreshold={0.5}
+  Answer (including related code snippets if available):`,
+        temperature: 0.1,
+        topP: 1,
+        frequencyPenalty: 0,
+        presencePenalty: 0,
+        maxTokens: 500,
+        sectionsMatchCount: 10,
+        sectionsMatchThreshold: 0.5,
+      }}
     >
       <Markprompt.DialogTrigger
         aria-label="Open Markprompt"
