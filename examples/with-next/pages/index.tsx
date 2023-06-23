f897cb88b3cf6164f7bb85421462f9aad72bdf8c
Nick Rutten <2504906+nickrttn@users.noreply.github.com>
2023-06-23 17:29:56 +0100

    Use `<Markprompt />` from `@markprompt/react` in `@markprompt/docusaurus-theme-search` and update examples (#47)
    * Use Markprompt in @markprompt/docusaurus-theme-search
    
    * Update Next.js and Docusaurus examples to use the headful Markprompt component
    
    * Update example dependencies
    
    * Build @markprompt/docusaurus-theme-search in CI
    
    * Add paths to @markprompt/docusaurus-theme-search tsconfig.json
    
    * Cancel previous runs of the CI workflow in the same branch
    
    * Add changesets
    
    * Add MARKPROMPT_PROJECT_KEY environment variable to CI build step
    
    * Don't render <Markprompt /> on the server in with-docusaurus-swizzled
    
    * Add build/ to .prettierignore
    
    * Add build/ to .eslintrc.js ignore

--- examples/with-next/pages/index.tsx
+++ examples/with-next/pages/index.tsx
@@ -4,7 +4,7 @@ import React, { ReactElement } from 'react';
 export default function IndexPage(): ReactElement {
   return (
     <Markprompt
-      projectKey={process.env.MARKPROMPT_PROJECT_KEY!}
+      projectKey={process.env.NEXT_PUBLIC_MARKPROMPT_PROJECT_KEY!}
       prompt={{
         iDontKnowMessage: 'Sorry, I am not sure how to answer that.',
         promptTemplate: `You are a very enthusiastic company representative who loves to help people! Given the following sections from the documentation (preceded by a section id), answer the question using only that information, output in Markdown format. If you are unsure and the answer is not explicitly written in the documentation, say "{{I_DONT_KNOW}}".
