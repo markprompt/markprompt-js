0c6a19bd76e1bb05c07198a49b4da6de74f30d9a
Michael Fester <michael.fester@gmail.com>
2023-07-07 07:30:07 -0700

    Improve error handling

--- examples/with-markprompt-web/src/main.ts
+++ examples/with-markprompt-web/src/main.ts
@@ -8,7 +8,7 @@ const el = document.querySelector('#markprompt');
 if (el && el instanceof HTMLElement) {
   markprompt(import.meta.env.VITE_PROJECT_API_KEY, el, {
     feedback: { enabled: true },
-    search: { enabled: false },
     trigger: { floating: true },
+    search: { enabled: true, apiUrl: 'http://api.localhost:3000/v1/search' },
   });
 }
