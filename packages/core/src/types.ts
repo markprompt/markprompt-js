cb2d619395e00342c2bb9ef5ae485f2b82ee1c3c
Michael Fester <michael.fester@gmail.com>
2023-07-13 11:51:06 -0700

    Split search prompt fix (#103)
    * Update exposed types
    
    * Add changeset
    
    * Fix build
    
    * Add unit tests

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -43,7 +43,7 @@ export type SearchResult = {
 
 export type SearchResultSection = {
   content?: string;
-  snippet: string;
+  snippet?: string;
 } & FileSectionReferenceSectionData;
 
 export type FileSectionReference = {
@@ -70,6 +70,5 @@ export type FileReferenceFileData = {
 
 export type SearchResultsResponse = {
   debug?: any;
-  project_id: string;
   data: SearchResult[];
 };
