95147f697c2db02cb6dae770955e77a5db4bc355
Nick Rutten <2504906+nickrttn@users.noreply.github.com>
2023-08-31 17:11:43 +0200

    Restore example

--- examples/with-markprompt-web/src/main.ts
+++ examples/with-markprompt-web/src/main.ts
@@ -8,14 +8,6 @@ const el = document.querySelector('#markprompt');
 if (el && el instanceof HTMLElement) {
   markprompt(import.meta.env.VITE_PROJECT_API_KEY, el, {
     feedback: { enabled: true },
-    search: {
-      enabled: true,
-      apiUrl: import.meta.env.VITE_MARKPROMPT_API_URL + '/v1/search',
-    },
-    chat: {
-      enabled: true,
-      apiUrl: import.meta.env.VITE_MARKPROMPT_API_URL + '/v1/chat',
-    },
-    open: true,
+    search: { enabled: true },
   } satisfies MarkpromptOptions);
 }
