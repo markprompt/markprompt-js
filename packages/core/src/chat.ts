37f1ba5ad60b153cfeac292a782ad9b8d97c1810
Nick Rutten <2504906+nickrttn@users.noreply.github.com>
2023-09-07 13:33:22 +0200

    Remove shared constants.ts

--- packages/core/src/chat.ts
+++ packages/core/src/chat.ts
@@ -1,18 +1,64 @@
 import defaults from 'defaults';
 
-import {
-  DEFAULT_COMPLETIONS_OPTIONS,
-  type CompletionsOptions,
-} from './constants.js';
-import type { FileSectionReference } from './types.js';
+import type { FileSectionReference, OpenAIModelId } from './types.js';
 import { isFileSectionReferences, parseEncodedJSONHeader } from './utils.js';
 
-export interface SubmitChatOptions extends CompletionsOptions {
+export interface SubmitChatOptions {
   /**
    * URL at which to fetch completions
    * @default "https://api.markprompt.com/v1/chat"
    * */
   apiUrl?: string;
+  /**
+   * Message returned when the model does not have an answer
+   * @default "Sorry, I am not sure how to answer that."
+   **/
+  iDontKnowMessage?: string;
+  /**
+   * The OpenAI model to use
+   * @default "gpt-3.5-turbo"
+   **/
+  model?: OpenAIModelId;
+  /**
+   * The system prompt
+   * @default "You are a very enthusiastic company representative who loves to help people!"
+   **/
+  systemPrompt?: string;
+  /**
+   * The model temperature
+   * @default 0.1
+   **/
+  temperature?: number;
+  /**
+   * The model top P
+   * @default 1
+   **/
+  topP?: number;
+  /**
+   * The model frequency penalty
+   * @default 0
+   **/
+  frequencyPenalty?: number;
+  /**
+   * The model present penalty
+   * @default 0
+   **/
+  presencePenalty?: number;
+  /**
+   * The max number of tokens to include in the response
+   * @default 500
+   * */
+  maxTokens?: number;
+  /**
+   * The number of sections to include in the prompt context
+   * @default 10
+   * */
+  sectionsMatchCount?: number;
+  /**
+   * The similarity threshold between the input question and selected sections
+   * @default 0.5
+   * */
+  sectionsMatchThreshold?: number;
   /**
    * AbortController signal
    * @default undefined
@@ -26,8 +72,17 @@ type DefaultSubmitChatOptions = Omit<Required<SubmitChatOptions>, 'signal'> &
 export const STREAM_SEPARATOR = '___START_RESPONSE_STREAM___';
 
 export const DEFAULT_SUBMIT_CHAT_OPTIONS = {
-  ...DEFAULT_COMPLETIONS_OPTIONS,
   apiUrl: 'https://api.markprompt.com/v1/chat',
+  frequencyPenalty: 0,
+  iDontKnowMessage: 'Sorry, I am not sure how to answer that.',
+  maxTokens: 500,
+  model: 'gpt-3.5-turbo',
+  presencePenalty: 0,
+  sectionsMatchCount: 10,
+  sectionsMatchThreshold: 0.5,
+  systemPrompt: `You are a very enthusiastic company representative who loves to help people!`,
+  temperature: 0.1,
+  topP: 1,
 } satisfies DefaultSubmitChatOptions;
 
 export interface ChatMessage {
