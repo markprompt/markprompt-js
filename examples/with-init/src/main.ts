9a9ae8115aa95a910381f5f692ac5f227e52641d
Nick Rutten <2504906+nickrttn@users.noreply.github.com>
2023-09-07 12:28:33 +0200

    Add options to with-init example

--- examples/with-init/src/main.ts
+++ examples/with-init/src/main.ts
@@ -3,6 +3,9 @@ import './style.css';
 // @ts-expect-error - Markprompt types have not been included here
 window.markprompt = {
   projectKey: import.meta.env.VITE_PROJECT_API_KEY,
+  options: {
+    search: { enabled: true },
+  },
 };
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
