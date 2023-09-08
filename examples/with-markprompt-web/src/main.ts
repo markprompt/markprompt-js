4087e3df2aeb2a0e980ee56e09e8b43a17be92d9
Michael Fester <michael.fester@gmail.com>
2023-09-07 21:49:29 -0700

    Add support for gpt-4-32k

--- examples/with-markprompt-web/src/main.ts
+++ examples/with-markprompt-web/src/main.ts
@@ -9,8 +9,5 @@ if (el && el instanceof HTMLElement) {
   markprompt(import.meta.env.VITE_PROJECT_API_KEY, el, {
     feedback: { enabled: true },
     search: { enabled: true },
-    chat: { enabled: true, apiUrl: 'http://api.localhost:3000/v1/chat' },
-    prompt: { apiUrl: 'http://api.localhost:3000/v1/chat' },
-    defaultView: 'chat',
   } satisfies MarkpromptOptions);
 }
--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -5,11 +5,7 @@ export type RequiredKeys<T, K extends keyof T> = Required<Pick<T, K>> &
 
 export type OpenAIChatCompletionsModelId =
   | 'gpt-4'
-  | 'gpt-4-0314'
   | 'gpt-4-32k'
-  | 'gpt-4-32k-0314'
-  | 'gpt-3.5-turbo'
-  | 'gpt-3.5-turbo-0301'
   | 'gpt-3.5-turbo';
 
 export type OpenAICompletionsModelId =
