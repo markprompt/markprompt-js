10a2f04760f31007520763c5d6d2bb2a53ad2ff3
Michael Fester <michael.fester@gmail.com>
2023-09-09 18:23:52 -0700

    Feedback callback (#181)
    * Add feedback callback
    
    * Update README
    
    * Ignore linting error in README
    
    * Add changeset

--- examples/with-markprompt-web/src/main.ts
+++ examples/with-markprompt-web/src/main.ts
@@ -9,6 +9,6 @@ if (el && el instanceof HTMLElement) {
   markprompt(import.meta.env.VITE_PROJECT_API_KEY, el, {
     feedback: { enabled: true },
     search: { enabled: true },
-    chat: { enabled: true },
+    chat: { enabled: true, apiUrl: `${import.meta.env.VITE_API_URL}/v1/chat` },
   } satisfies MarkpromptOptions);
 }
