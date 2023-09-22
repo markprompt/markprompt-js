9866b67f4223ee1cb3e5ba4aa457ed20ed86b485
Nick Rutten <2504906+nickrttn@users.noreply.github.com>
2023-09-22 15:21:59 +0200

    Fix malformatted changelog

--- prettier.config.js
+++ prettier.config.js
@@ -3,5 +3,20 @@ module.exports = {
   semi: true,
   trailingComma: 'all',
   singleQuote: true,
-  proseWrap: 'always',
+  proseWrap: 'never',
+  overrides: [
+    {
+      files: ['*.md', '*.mdx'],
+      options: {
+        printWidth: 80,
+        proseWrap: 'always',
+      },
+    },
+    {
+      files: ['**/.changeset/*.md', '**/CHANGELOG.MD'],
+      options: {
+        proseWrap: 'never',
+      },
+    },
+  ],
 };
