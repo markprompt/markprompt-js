a50cde529d2addfb3a98c9906627a45a6eb6828a
Michael Fester <michael.fester@gmail.com>
2023-10-22 19:23:52 -0700

    Fix build

--- examples/with-next/pages/index.tsx
+++ examples/with-next/pages/index.tsx
@@ -4,10 +4,9 @@ import React, { ReactElement } from 'react';
 export default function IndexPage(): ReactElement {
   return (
     <Markprompt
-      projectKey={process.env.NEXT_PUBLIC_MARKPROMPT_PROJECT_KEY!}
+      projectKey="YOUR-PROJECT-KEY"
       chat={{
         enabled: true,
-        apiUrl: `${process.env.NEXT_PUBLIC_MARKPROMPT_API_URL}/v1/chat`,
         iDontKnowMessage: 'Sorry, I am not sure how to answer that.',
         systemPrompt:
           'You are a very enthusiastic company representative who loves to help people!',
