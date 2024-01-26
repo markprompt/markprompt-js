44e17a6eccff8e7c35547526b2a795832d94aae4
Nick Rutten <2504906+nickrttn@users.noreply.github.com>
2024-01-26 20:59:35 +0100

    Update tests

--- packages/core/src/chat.test.ts
+++ packages/core/src/chat.test.ts
@@ -14,11 +14,9 @@ import {
 } from 'vitest';
 
 import {
-  DEFAULT_SUBMIT_CHAT_GENERATOR_OPTIONS,
   DEFAULT_SUBMIT_CHAT_OPTIONS,
   submitChat,
-  submitChatGenerator,
-  SubmitChatGeneratorOptions,
+  SubmitChatOptions,
   SubmitChatReturn,
   SubmitChatYield,
 } from './index.js';
@@ -28,26 +26,69 @@ describe('submitChat', () => {
   const encoder = new TextEncoder();
   let markpromptData: unknown = '';
   const markpromptDebug = '';
-  let response: string[] = [];
-  let status = 200;
+  let response: string[] | string | object = [];
   let request: RestRequest;
-  let requestBody: unknown = {};
+  let requestBody: SubmitChatOptions = {};
   let stream: ReadableStream;
+  let malformattedJson = false;
+
+  let status = 200;
 
   const server = setupServer(
     rest.post(DEFAULT_SUBMIT_CHAT_OPTIONS.apiUrl!, async (req, res, ctx) => {
       request = req;
       requestBody = await req.json();
+
+      if (status >= 400) {
+        return res(
+          ctx.status(status),
+          ctx.text(
+            typeof response !== 'string' ? JSON.stringify(response) : response,
+          ),
+        );
+      }
+
+      if (requestBody?.stream === false) {
+        return res(
+          ctx.status(status),
+          ctx.set('Content-Type', 'application/json'),
+          ctx.set(
+            'x-markprompt-data',
+            encoder.encode(JSON.stringify(markpromptData)).toString(),
+          ),
+          ctx.set(
+            'x-markprompt-debug-info',
+            encoder.encode(JSON.stringify(markpromptDebug)).toString(),
+          ),
+          ctx.json(response),
+        );
+      }
+
       stream = new ReadableStream({
         start(controller) {
-          for (const chunk of response) {
-            controller.enqueue(encoder.encode(chunk));
+          if (Array.isArray(response)) {
+            let i = 0;
+            for (const chunk of response) {
+              controller.enqueue(
+                encoder.encode(
+                  formatEvent({
+                    data: malformattedJson ? chunk : getChunk(chunk, i),
+                  }),
+                ),
+              );
+              i++;
+            }
+            controller.enqueue(encoder.encode(formatEvent({ data: '[DONE]' })));
+          } else {
+            controller.enqueue(encoder.encode(JSON.stringify(response)));
           }
+
           controller?.close();
         },
       });
 
       return res(
+        ctx.delay('real'),
         ctx.status(status),
         ctx.set(
           'x-markprompt-data',
@@ -60,408 +101,6 @@ describe('submitChat', () => {
         ctx.body(stream),
       );
     }),
-  );
-
-  const onAnswerChunk = vi.fn().mockReturnValue(true);
-  const onReferences = vi.fn();
-  const onPromptId = vi.fn();
-  const onConversationId = vi.fn();
-  const onError = vi.fn();
-
-  beforeAll(() => {
-    server.listen({ onUnhandledRequest: 'error' });
-  });
-
-  afterAll(() => {
-    server.close();
-    vi.restoreAllMocks();
-  });
-
-  afterEach(() => {
-    response = [];
-    requestBody = {};
-    markpromptData = '';
-    status = 200;
-    server.resetHandlers();
-    vi.resetAllMocks();
-  });
-
-  test('require projectKey', async () => {
-    await expect(() =>
-      // @ts-expect-error We test a missing project key.
-      submitChat([]),
-    ).rejects.toThrowError('A projectKey is required');
-  });
-
-  test('don’t make requests if the prompt is empty', async () => {
-    await submitChat(
-      [],
-      'testKey',
-      onAnswerChunk,
-      onReferences,
-      onConversationId,
-      onPromptId,
-      onError,
-    );
-
-    expect(request).toBeUndefined();
-    expect(onAnswerChunk).not.toHaveBeenCalled();
-    expect(onReferences).not.toHaveBeenCalled();
-    expect(onError).not.toHaveBeenCalled();
-  });
-
-  test('endpoint is called with the default options if none are provided', async () => {
-    await submitChat(
-      [{ content: 'How much is 1+2?', role: 'user' }],
-      'testKey',
-      onAnswerChunk,
-      onReferences,
-      onConversationId,
-      onPromptId,
-      onError,
-    );
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { apiUrl, ...rest } = DEFAULT_SUBMIT_CHAT_OPTIONS;
-
-    expect(requestBody).toStrictEqual({
-      messages: [{ content: 'How much is 1+2?', role: 'user' }],
-      projectKey: 'testKey',
-      ...rest,
-    });
-  });
-
-  test('make a request', async () => {
-    onAnswerChunk.mockReturnValue(true);
-
-    response = ['According to my calculator ', '1 + 2 = 3'];
-
-    await submitChat(
-      [{ content: 'How much is 1+2?', role: 'user' }],
-      'testKey',
-      onAnswerChunk,
-      onReferences,
-      onConversationId,
-      onPromptId,
-      onError,
-    );
-
-    expect(request).toBeDefined();
-    expect(onAnswerChunk.mock.calls).toStrictEqual([
-      ['According to my calculator '],
-      ['1 + 2 = 3'],
-    ]);
-    expect(onError).not.toHaveBeenCalled();
-  });
-
-  test('handle error status code', async () => {
-    status = 500;
-    response = ['Internal Server Error'];
-
-    await submitChat(
-      [{ content: 'How much is 1+2?', role: 'user' }],
-      'testKey',
-      onAnswerChunk,
-      onReferences,
-      onConversationId,
-      onPromptId,
-      onError,
-    );
-
-    expect(request).toBeDefined();
-    expect(onAnswerChunk.mock.calls).toStrictEqual([
-      [DEFAULT_SUBMIT_CHAT_OPTIONS.iDontKnowMessage],
-    ]);
-    expect(onReferences).not.toHaveBeenCalled();
-    expect(onError.mock.calls).toStrictEqual([
-      [new Error('Internal Server Error')],
-    ]);
-  });
-
-  test('ignore invalid references', async () => {
-    onAnswerChunk.mockReturnValue(true);
-
-    response = ['According to my calculator ', '1 + 2 = 3'];
-
-    await submitChat(
-      [{ content: 'How much is 1+2?', role: 'user' }],
-      'testKey',
-      onAnswerChunk,
-      onReferences,
-      onConversationId,
-      onPromptId,
-      onError,
-    );
-
-    expect(request).toBeDefined();
-    expect(onAnswerChunk.mock.calls).toStrictEqual([
-      ['According to my calculator '],
-      ['1 + 2 = 3'],
-    ]);
-    expect(onReferences).not.toHaveBeenCalled();
-    expect(onError).not.toHaveBeenCalled();
-  });
-
-  test('stop if onAnswerChunk returns false', async () => {
-    onAnswerChunk.mockReturnValue(false);
-
-    response = ['According to my calculator ', '1 + 2 = 3'];
-
-    await submitChat(
-      [{ content: 'How much is 1+2?', role: 'user' }],
-      'testKey',
-      onAnswerChunk,
-      onReferences,
-      onConversationId,
-      onPromptId,
-      onError,
-    );
-
-    expect(request).toBeDefined();
-    expect(onAnswerChunk.mock.calls).toStrictEqual([
-      ['According to my calculator '],
-    ]);
-    expect(onReferences).not.toHaveBeenCalled();
-    expect(onError).not.toHaveBeenCalled();
-  });
-
-  test('calls back user-provided onReferences', async () => {
-    const references = [
-      {
-        file: { path: '/page1', source: { type: 'file-upload' } },
-        meta: { leadHeading: { value: 'Page 1' } },
-      },
-    ];
-
-    markpromptData = { references };
-
-    response = ['According to my calculator ', '1 + 2 = 3'];
-
-    await submitChat(
-      [{ content: 'How much is 1+2?', role: 'user' }],
-      'testKey',
-      onAnswerChunk,
-      onReferences,
-      onConversationId,
-      onPromptId,
-      onError,
-    );
-
-    expect(request).toBeDefined();
-    expect(onAnswerChunk).toHaveBeenCalled();
-    expect(onReferences).toHaveBeenCalledWith(references);
-    expect(onError).not.toHaveBeenCalled();
-  });
-
-  test('calls back user-provided onPromptId', async () => {
-    const promptId = 'test-id';
-
-    markpromptData = { promptId };
-
-    response = ['According to my calculator ', '1 + 2 = 3'];
-
-    await submitChat(
-      [{ content: 'How much is 1+2?', role: 'user' }],
-      'testKey',
-      onAnswerChunk,
-      onReferences,
-      onConversationId,
-      onPromptId,
-      onError,
-    );
-
-    expect(request).toBeDefined();
-    expect(onAnswerChunk).toHaveBeenCalled();
-    expect(onPromptId).toHaveBeenCalledWith(promptId);
-    expect(onError).not.toHaveBeenCalled();
-  });
-
-  test('calls back user-provided onConversationId', async () => {
-    const conversationId = 'test-id';
-
-    markpromptData = { conversationId };
-
-    response = ['According to my calculator ', '1 + 2 = 3'];
-
-    await submitChat(
-      [{ content: 'How much is 1+2?', role: 'user' }],
-      'testKey',
-      onAnswerChunk,
-      onReferences,
-      onConversationId,
-      onPromptId,
-      onError,
-    );
-
-    expect(request).toBeDefined();
-    expect(onAnswerChunk).toHaveBeenCalled();
-    expect(onConversationId).toHaveBeenCalledWith(conversationId);
-    expect(onError).not.toHaveBeenCalled();
-  });
-
-  test('expect onError to be called when an error occurs', async () => {
-    const mockFetch = vi.spyOn(global, 'fetch').mockImplementation(() => {
-      throw new Error('test');
-    });
-
-    try {
-      response = ['According to my calculator ', '1 + 2 = 3'];
-
-      await submitChat(
-        [{ content: 'How much is 1+2?', role: 'user' }],
-        'testKey',
-        onAnswerChunk,
-        onConversationId,
-        onReferences,
-        onPromptId,
-        onError,
-      );
-
-      expect(request).toBeDefined();
-      expect(onError).toHaveBeenCalledWith(new Error('test'));
-    } finally {
-      mockFetch.mockRestore();
-    }
-  });
-
-  test('expect to log the error when debug is enabled and an error occurs', async () => {
-    const mockFetch = vi.spyOn(global, 'fetch').mockImplementation(() => {
-      throw new Error('test');
-    });
-
-    try {
-      response = ['According to my calculator ', '1 + 2 = 3'];
-
-      await submitChat(
-        [{ content: 'How much is 1+2?', role: 'user' }],
-        'testKey',
-        onAnswerChunk,
-        onConversationId,
-        onReferences,
-        onPromptId,
-        onError,
-        { debug: true },
-      );
-
-      expect(request).toBeDefined();
-      expect(onError).toHaveBeenCalledWith(new Error('test'));
-    } finally {
-      mockFetch.mockRestore();
-    }
-  });
-
-  test('expect onError to be called when a non-error is thrown', async () => {
-    const mockFetch = vi.spyOn(global, 'fetch').mockImplementation(() => {
-      throw 'test';
-    });
-
-    try {
-      response = ['According to my calculator ', '1 + 2 = 3'];
-
-      await submitChat(
-        [{ content: 'How much is 1+2?', role: 'user' }],
-        'testKey',
-        onAnswerChunk,
-        onConversationId,
-        onReferences,
-        onPromptId,
-        onError,
-      );
-
-      expect(request).toBeDefined();
-      expect(onError).toHaveBeenCalledWith(new Error('test'));
-    } finally {
-      mockFetch.mockRestore();
-    }
-  });
-});
-
-describe('submitChatGenerator', () => {
-  const encoder = new TextEncoder();
-  let markpromptData: unknown = '';
-  const markpromptDebug = '';
-  let response: string[] | string | object = [];
-  let request: RestRequest;
-  let requestBody: SubmitChatGeneratorOptions = {};
-  let stream: ReadableStream;
-  let malformattedJson = false;
-
-  let status = 200;
-
-  const server = setupServer(
-    rest.post(
-      DEFAULT_SUBMIT_CHAT_GENERATOR_OPTIONS.apiUrl!,
-      async (req, res, ctx) => {
-        request = req;
-        requestBody = await req.json();
-
-        if (status >= 400) {
-          return res(
-            ctx.status(status),
-            ctx.text(
-              typeof response !== 'string'
-                ? JSON.stringify(response)
-                : response,
-            ),
-          );
-        }
-
-        if (requestBody?.stream === false) {
-          return res(
-            ctx.status(status),
-            ctx.set('Content-Type', 'application/json'),
-            ctx.set(
-              'x-markprompt-data',
-              encoder.encode(JSON.stringify(markpromptData)).toString(),
-            ),
-            ctx.set(
-              'x-markprompt-debug-info',
-              encoder.encode(JSON.stringify(markpromptDebug)).toString(),
-            ),
-            ctx.json(response),
-          );
-        }
-
-        stream = new ReadableStream({
-          start(controller) {
-            if (Array.isArray(response)) {
-              let i = 0;
-              for (const chunk of response) {
-                controller.enqueue(
-                  encoder.encode(
-                    formatEvent({
-                      data: malformattedJson ? chunk : getChunk(chunk, i),
-                    }),
-                  ),
-                );
-                i++;
-              }
-              controller.enqueue(
-                encoder.encode(formatEvent({ data: '[DONE]' })),
-              );
-            } else {
-              controller.enqueue(encoder.encode(JSON.stringify(response)));
-            }
-
-            controller?.close();
-          },
-        });
-
-        return res(
-          ctx.delay('real'),
-          ctx.status(status),
-          ctx.set(
-            'x-markprompt-data',
-            encoder.encode(JSON.stringify(markpromptData)).toString(),
-          ),
-          ctx.set(
-            'x-markprompt-debug-info',
-            encoder.encode(JSON.stringify(markpromptDebug)).toString(),
-          ),
-          ctx.body(stream),
-        );
-      },
-    ),
   );
 
   beforeAll(() => {
@@ -486,7 +125,7 @@ describe('submitChatGenerator', () => {
   test('require projectKey', async () => {
     try {
       // @ts-expect-error We test a missing project key.
-      for await (const _chunk of submitChatGenerator([])) {
+      for await (const _chunk of submitChat([])) {
         // do nothing
       }
 
@@ -498,7 +137,7 @@ describe('submitChatGenerator', () => {
 
   test('don’t make requests if the prompt is empty', async () => {
     const chunks: unknown[] = [];
-    for await (const chunk of submitChatGenerator([], 'test-key')) {
+    for await (const chunk of submitChat([], 'test-key')) {
       chunks.push(chunk);
     }
 
@@ -510,7 +149,7 @@ describe('submitChatGenerator', () => {
 
     const chunks: unknown[] = [];
 
-    for await (const chunk of submitChatGenerator(
+    for await (const chunk of submitChat(
       [{ content: 'How much is 1+2?', role: 'user' }],
       'testKey',
     )) {
@@ -523,14 +162,14 @@ describe('submitChatGenerator', () => {
   });
 
   test('endpoint is called with the default options if none are provided', async () => {
-    for await (const _chunk of submitChatGenerator(
+    for await (const _chunk of submitChat(
       [{ content: 'How much is 1+2?', role: 'user' }],
       'testKey',
     )) {
       // do nothing
     }
 
-    const { apiUrl: _apiUrl, ...rest } = DEFAULT_SUBMIT_CHAT_GENERATOR_OPTIONS;
+    const { apiUrl: _apiUrl, ...rest } = DEFAULT_SUBMIT_CHAT_OPTIONS;
 
     expect(requestBody).toStrictEqual({
       messages: [{ content: 'How much is 1+2?', role: 'user' }],
@@ -543,7 +182,7 @@ describe('submitChatGenerator', () => {
     response = ['According to my calculator ', '1 + 2 = 3'];
 
     const chunks: SubmitChatYield[] = [];
-    for await (const chunk of submitChatGenerator(
+    for await (const chunk of submitChat(
       [{ content: 'How much is 1+2?', role: 'user' }],
       'testKey',
     )) {
@@ -564,7 +203,7 @@ describe('submitChatGenerator', () => {
     response = ['According to my calculator ', '1 + 2 = 3'];
 
     const chunks: SubmitChatYield[] = [];
-    for await (const chunk of submitChatGenerator(
+    for await (const chunk of submitChat(
       [{ content: 'How much is 1+2?', role: 'user' }],
       'testKey',
     )) {
@@ -583,7 +222,7 @@ describe('submitChatGenerator', () => {
     response = ['According to my calculator ', '1 + 2 = 3'];
 
     const chunks: SubmitChatYield[] = [];
-    for await (const chunk of submitChatGenerator(
+    for await (const chunk of submitChat(
       [{ content: 'How much is 1+2?', role: 'user' }],
       'testKey',
     )) {
@@ -603,7 +242,7 @@ describe('submitChatGenerator', () => {
     response = ['According to my calculator ', '1 + 2 = 3'];
 
     const chunks: SubmitChatYield[] = [];
-    for await (const chunk of submitChatGenerator(
+    for await (const chunk of submitChat(
       [{ content: 'How much is 1+2?', role: 'user' }],
       'testKey',
     )) {
@@ -622,7 +261,7 @@ describe('submitChatGenerator', () => {
     });
 
     try {
-      for await (const _chunk of submitChatGenerator(
+      for await (const _chunk of submitChat(
         [{ content: 'How much is 1+2?', role: 'user' }],
         'testKey',
       )) {
@@ -642,7 +281,7 @@ describe('submitChatGenerator', () => {
     response = 'Internal Server Error';
 
     try {
-      for await (const _chunk of submitChatGenerator(
+      for await (const _chunk of submitChat(
         [{ content: 'How much is 1+2?', role: 'user' }],
         'testKey',
       )) {
@@ -664,7 +303,7 @@ describe('submitChatGenerator', () => {
 
     const chunks: SubmitChatYield[] = [];
     try {
-      for await (const chunk of submitChatGenerator(
+      for await (const chunk of submitChat(
         [{ content: 'How much is 1+2?', role: 'user' }],
         'testKey',
       )) {
@@ -704,7 +343,7 @@ describe('submitChatGenerator', () => {
 
     markpromptData = { conversationId, promptId, references };
 
-    for await (const json of submitChatGenerator(
+    for await (const json of submitChat(
       [{ content: 'How much is 1+2?', role: 'user' }],
       'testKey',
       { stream: false },
@@ -725,7 +364,7 @@ describe('submitChatGenerator', () => {
     };
 
     try {
-      for await (const _json of submitChatGenerator(
+      for await (const _json of submitChat(
         [{ content: 'How much is 1+2?', role: 'user' }],
         'testKey',
         { stream: false },
