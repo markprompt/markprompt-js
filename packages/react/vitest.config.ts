28daee12155983656231627c186277101feb567b
Nick Rutten <2504906+nickrttn@users.noreply.github.com>
2024-02-02 12:19:32 +0100

    Fix test config

--- packages/react/vitest.config.ts
+++ packages/react/vitest.config.ts
@@ -2,7 +2,8 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineProject } from 'vitest/config';
 
 export default defineProject({
-  plugins: [tsconfigPaths({ ignoreConfigErrors: true })],
+  // for some reason this
+  plugins: [tsconfigPaths({ projects: ['./tsconfig.json'] })],
   test: {
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts'],
