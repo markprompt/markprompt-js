9edac9ecf8bb27bb11526d0e0b6d32bc03898030
Michael Fester <michael.fester@gmail.com>
2024-02-16 14:31:37 -0800

    Remove params

--- examples/chatgpt-next/pages/index.tsx
+++ examples/chatgpt-next/pages/index.tsx
@@ -1,7 +1,7 @@
-import { ReactElement, useEffect } from 'react';
+import { ReactElement } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
-import { Markprompt, openMarkprompt } from '@markprompt/react';
+import { Markprompt } from '@markprompt/react';
 
 export default function IndexPage(): ReactElement {
   return (
@@ -12,7 +12,6 @@ export default function IndexPage(): ReactElement {
       <div style={{ width: '100%', height: '100vh' }}>
         <Markprompt
           projectKey={process.env.NEXT_PUBLIC_MARKPROMPT_PROJECT_KEY!}
-          defaultView="search"
           display="plain"
           chat={{
             enabled: true,
@@ -26,34 +25,6 @@ export default function IndexPage(): ReactElement {
           }}
           feedback={{ enabled: true }}
           references={{ display: 'end' }}
-          // search={{
-          //   enabled: true,
-          //   askLabel: 'Ask Acme',
-          //   defaultView: {
-          //     searchesHeading: 'Recommended for you',
-          //     searches: [
-          //       { title: 'Welcome to Acme', href: '/' },
-          //       { title: 'Get Started', href: '/' },
-          //       { title: 'Onboarding', href: '/' },
-          //       { title: 'Payments', href: '/' },
-          //       { title: 'User Management', href: '/' },
-          //       { title: 'Acme CLI', href: '/' },
-          //       { title: 'How to build an Acme app', href: '/' },
-          //       { title: 'How to setup authentication', href: '/' },
-          //       { title: 'How to invite users', href: '/' },
-          //     ],
-          //   },
-          //   provider: {
-          //     name: 'algolia',
-          //     apiKey: process.env.NEXT_PUBLIC_ALGOLIA_API_KEY!,
-          //     appId: process.env.NEXT_PUBLIC_ALGOLIA_APP_ID!,
-          //     indexName: process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME!,
-          //   },
-          //   getHref: (result: any) => result.href,
-          //   getHeading: (result: any) => result.heading,
-          //   getTitle: (result) => result.content || undefined,
-          // }}
-          close={{ visible: false }}
           linkAs={Link}
         ></Markprompt>
       </div>
