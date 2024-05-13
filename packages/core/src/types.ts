6d1382b2ea6ae2a869b7cdf05d7dfa6fb42f86d6
Michael Fester <michael.fester@gmail.com>
2024-05-13 12:12:18 -0700

    Add support for GPT4o

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -25,6 +25,7 @@ export const OPENAI_CHAT_COMPLETIONS_MODELS = [
   'gpt-3.5-turbo-1106',
   'gpt-3.5-turbo-16k-0613',
   'gpt-4',
+  'gpt-4o',
   'gpt-4-32k',
   'gpt-4-1106-preview',
   'gpt-4-turbo-preview',
