e67efbbe8e3922f3805b0b15322b6825e54b4f7b
Michael Fester <michael.fester@gmail.com>
2024-09-23 15:13:13 -0700

    Remove rule

--- packages/react/src/types.ts
+++ packages/react/src/types.ts
@@ -30,7 +30,6 @@ type PropsToOmit<C extends ElementType, P> = keyof (AsProp<C> & P);
 // This is the first reusable type utility we built
 export type PolymorphicComponentProp<
   C extends ElementType,
-  // eslint-disable-next-line @typescript-eslint/ban-types
   Props = {},
 > = PropsWithChildren<Props & AsProp<C>> &
   Omit<ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>;
@@ -38,7 +37,6 @@ export type PolymorphicComponentProp<
 // This is a new type utility with ref!
 export type PolymorphicComponentPropWithRef<
   C extends ElementType,
-  // eslint-disable-next-line @typescript-eslint/ban-types
   Props = {},
 > = PolymorphicComponentProp<C, Props> & { ref?: PolymorphicRef<C> };
 
