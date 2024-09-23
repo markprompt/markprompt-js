b27051071fbc5220433158370bc6cfcd5f687bdf
Michael Fester <michael.fester@gmail.com>
2024-09-23 14:21:06 -0700

    Fix linting

--- packages/react/src/types.ts
+++ packages/react/src/types.ts
@@ -30,15 +30,15 @@ type PropsToOmit<C extends ElementType, P> = keyof (AsProp<C> & P);
 // This is the first reusable type utility we built
 export type PolymorphicComponentProp<
   C extends ElementType,
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  // eslint-disable-next-line @typescript-eslint/ban-types
   Props = {},
-> = PropsWithChildren<Props & AsProp<C>> &
+  > = PropsWithChildren<Props & AsProp<C>> &
   Omit<ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>;
 
-// This is a new type utility with ref!
-export type PolymorphicComponentPropWithRef<
+  // This is a new type utility with ref!
+  export type PolymorphicComponentPropWithRef<
   C extends ElementType,
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  // eslint-disable-next-line @typescript-eslint/ban-types
   Props = {},
 > = PolymorphicComponentProp<C, Props> & { ref?: PolymorphicRef<C> };
 
