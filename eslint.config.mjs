fa900bd968dff9f4a63b1bdad95bf92dfd82905d
Nick Rutten <2504906+nickrttn@users.noreply.github.com>
2024-10-18 13:34:47 +0200

    update configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 import { configs } from '@markprompt/eslint-config';
 
 export default [
+  {
+    ignores: ['coverage/', 'examples/', 'packages/'],
+  },
   ...configs.base(import.meta.url, ['test/utils.ts']),
   {
     rules: {
