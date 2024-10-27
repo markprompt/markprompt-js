9b0fe5ae8139c4bf2742480f49d2e3c274672db8
Nick Rutten <2504906+nickrttn@users.noreply.github.com>
2024-10-27 10:56:53 +0100

    update eslint config

--- packages/eslint-config/src/astro.ts
+++ packages/eslint-config/src/astro.ts
@@ -1,7 +1,7 @@
 import type { Linter } from 'eslint';
-import astroPlugin from 'eslint-plugin-astro';
+import { configs } from 'eslint-plugin-astro';
 
 export const astro: Linter.Config[] = [
-  ...astroPlugin.configs.recommended,
-  ...astroPlugin.configs['jsx-a11y-recommended'],
+  ...configs.recommended,
+  ...configs['jsx-a11y-recommended'],
 ];
--- packages/eslint-config/src/react.ts
+++ packages/eslint-config/src/react.ts
@@ -1,6 +1,4 @@
-/* eslint-disable import-x/no-named-as-default-member */
 import type { ESLint, Linter } from 'eslint';
-import importX from 'eslint-plugin-import-x';
 import pluginReact from 'eslint-plugin-react';
 import globals from 'globals';
 
@@ -9,7 +7,6 @@ export const react = [
     name: 'react',
     ...pluginReact.configs.flat.recommended,
     ...pluginReact.configs.flat['jsx-runtime'],
-    ...importX.flatConfigs.react,
     plugins: { react: pluginReact as ESLint.Plugin },
     languageOptions: {
       ...pluginReact.configs.flat.recommended.languageOptions,
