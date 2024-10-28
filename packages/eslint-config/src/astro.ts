d3c8e9188fec635aa7463419f7070dd8980e1335
Nick Rutten <2504906+nickrttn@users.noreply.github.com>
2024-10-28 14:54:54 +0100

    Configure eslint-plugin-import-x for Astro

--- packages/eslint-config/src/astro.ts
+++ packages/eslint-config/src/astro.ts
@@ -2,6 +2,18 @@ import type { Linter } from 'eslint';
 import { configs } from 'eslint-plugin-astro';
 
 export const astro: Linter.Config[] = [
-  ...configs.recommended,
+  ...configs['flat/recommended'],
   ...configs['jsx-a11y-recommended'],
+  {
+    files: ['**/*.astro'],
+    settings: {
+      'import-x/extensions': ['.astro'],
+      'import-x/parsers': {
+        'astro-eslint-parser': ['.astro'],
+      },
+    },
+    rules: {
+      'import-x/default': 'off',
+    },
+  },
 ];
