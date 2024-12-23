f3fedf621861b8ee4edc97e31724ae38e9ddf6a0
Elliot Dauber <elliotkdauber@gmail.com>
2024-12-22 19:53:42 -0800

    update vite config

--- examples/with-ticket-creation/vite.config.js
+++ examples/with-ticket-creation/vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
+import path from 'node:path';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@markprompt/core': path.resolve(__dirname, '../../packages/core/dist'),
+    },
+  },
   optimizeDeps: {
     exclude: ['@markprompt/web', '@markprompt/css', '@markprompt/react'],
   },
