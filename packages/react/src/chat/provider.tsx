56d88598d2d085fd7506f8ccce0f06750b730cd5
Elliot Dauber <67391073+elliotdauber@users.noreply.github.com>
2025-01-08 10:39:34 -0800

    create new chat store when store key changes (#484)
    * create new chat store when store key changes
    
    * up
    
    * up

--- packages/react/src/chat/provider.tsx
+++ packages/react/src/chat/provider.tsx
@@ -24,28 +24,37 @@ export function ChatProvider(props: ChatProviderProps): JSX.Element {
     headers,
   } = props;
 
-  const store = useRef<ChatStore>(null);
+  const store = useRef<{
+    storeKey: string | undefined;
+    store: ChatStore | null;
+  }>({
+    storeKey: undefined,
+    store: null,
+  });
 
-  if (!store.current) {
-    store.current = createChatStore({
-      apiUrl,
-      headers,
-      projectKey,
-      chatOptions,
-      debug,
-      persistChatHistory: chatOptions?.history,
+  if (!store.current?.store || store.current?.storeKey !== storeKey) {
+    store.current = {
       storeKey,
-    });
+      store: createChatStore({
+        apiUrl,
+        headers,
+        projectKey,
+        chatOptions,
+        debug,
+        persistChatHistory: chatOptions?.history,
+        storeKey,
+      }),
+    };
   }
 
   // update chat options when they change
   useEffect(() => {
     if (!chatOptions) return;
-    store.current?.getState().setOptions(chatOptions);
+    store.current?.store?.getState().setOptions(chatOptions);
   }, [chatOptions]);
 
   return (
-    <ChatContext.Provider value={store.current}>
+    <ChatContext.Provider value={store.current?.store}>
       {children}
     </ChatContext.Provider>
   );
