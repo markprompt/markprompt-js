7636bcbb41327ebc0585b601dc29959a8cbbb064
Nick Rutten <2504906+nickrttn@users.noreply.github.com>
2025-01-28 12:47:30 +0100

    up

--- packages/react/src/chat/MessagePrompt.tsx
+++ packages/react/src/chat/MessagePrompt.tsx
@@ -41,23 +41,32 @@ export function MessagePrompt(props: MessagePromptProps): JSX.Element {
       {/* Use a div instead of an H3, as often websites will have CSS rules
           that override the styles of such components. */}
       {Array.isArray(message?.content) ? (
-        message.content.map((part) =>
-          part.type === 'text' ? (
-            <div
-              key={JSON.stringify(part)}
-              className="MarkpromptMessagePromptText"
-            >
-              {part.text}
-            </div>
-          ) : (
-            <div
-              key={JSON.stringify(part)}
-              className="MarkpromptMessagePromptImage"
-            >
-              <img src={part.image_url.url} alt="user uploaded" />
-            </div>
-          ),
-        )
+        message.content.map((part) => {
+          if (part.type === 'text') {
+            return (
+              <div
+                key={JSON.stringify(part)}
+                className="MarkpromptMessagePromptText"
+              >
+                {part.text}
+              </div>
+            );
+          }
+
+          if (part.type === 'image_url') {
+            return (
+              <div
+                key={JSON.stringify(part)}
+                className="MarkpromptMessagePromptImage"
+              >
+                <img src={part.image_url.url} alt="user uploaded" />
+              </div>
+            );
+          }
+
+          // todo: add support for type = 'input_audio'
+          return null;
+        })
       ) : (
         <div className="MarkpromptMessagePromptText">
           {message?.content ?? ''}
